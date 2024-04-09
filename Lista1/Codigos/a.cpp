68f9ca46ee6cbcac94b7166282cd030c831c0131
Joao Artur Leles <jalxpinheiro@gmail.com>
2024-04-09 14:44:51 -0300

    Adicionando questão A lista 1

---
+++ Lista1/Codigos/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int a,b,d;
+    double rad, a2, b2;
+    cin >> a >> b >> d;
+    // transforma um angulo de graus pra radianos
+    rad = d * (M_PI /180);
+    // rotação dos pontos dado um ângulo
+    a2 = (a*cos(rad)) - (b*sin(rad)); 
+    b2 = (a*sin(rad)) + (b*cos(rad));
+    cout << fixed << setprecision(15);
+    cout << a2 << ' ' << b2;
+    return 0;
+}
