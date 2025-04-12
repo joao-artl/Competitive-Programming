a2a7bf61ba5891d06ad008705a2a702153a73333
joao-artl <jalxpinheiro@gmail.com>
2025-04-12 09:18:13 -0300

    adicionando questão

---
+++ AtCoder/statusCode.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int valor;
+    cin >> valor;
+    if(valor >= 200 && valor <= 299){
+        cout << "Success" << endl;
+    }
+    else{
+        cout << "Failure" << endl;
+    }
+    return 0;
+}
