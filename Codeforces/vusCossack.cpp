a1e51ec936a20654394b2715a5f1d5fc233f9b0b
joao-artl <jalxpinheiro@gmail.com>
2025-09-22 11:04:13 -0300

    add: questão do codeforces

---
+++ Codeforces/vusCossack.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+
+    int n, m , k;
+    cin >> n >> m >> k;
+    if(n > m || n > k)
+    {
+        cout << "No" << '\n';
+    }
+    else
+    {
+        cout << "Yes" << '\n';
+    }
+
+    return 0;
+}
