af7d115fac20c405670e782c68455004ba71ba9c
joao-artl <jalxpinheiro@gmail.com>
2025-09-30 12:39:30 -0300

    add: adiciona documentação sobre o vector

---
+++ Notebook/algorithms/Estrutura de dados/vector.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    vector<int> vet(n, 0); // Cria vetor de tamanho 5, com todos valores = 0
+
+    vet.push_back(5); // Adiciona valor no fim do vetor
+    vet.pop_back(); // Remove ultimo valor do vetor
+    vet.size(); // Retorna tamanho do vetor
+    vet.clear(); // Remove todos os elementos mas não libera a memória alocada
+
+    vector<vector<int>> matriz(n, vector<int>(n, -2)); // Declara Matriz de tamanho NxN
+    for (int i = 0; i < n; i++) {           // percorre as linhas
+        for (int j = 0; j < n; j++) {       // percorre as colunas
+            cout << matriz[i][j];    // acessa elemento [i][j]
+        }
+    }
+}
--- Notebook/algorithms/vector/a.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    cout << "ola" << '\n';
-    return 0;
-}
