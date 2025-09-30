df99f7426df18f5355d912c7a9f62c2e5a9c3da6
joao-artl <jalxpinheiro@gmail.com>
2025-09-30 12:43:33 -0300

    add: adiciona documentação  inicial sobre pilha

---
+++ Notebook/algorithms/Estrutura de dados/pilha.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    stack<int> st;
+
+    st.push(3); // Adiciona elemento na pilha
+    st.pop(); // Remove elemento no topo
+    st.empty(); // Verifica se a pilha está vazia
+    st.size(); // Retorna tamanho da pilha
+    st.top(); // Retorna valor no topo da pilha
+
+}
