6061ca30c94156f210899548e178626d0f52b700
joao-artl <jalxpinheiro@gmail.com>
2025-10-01 00:03:02 -0300

    add: documentação de pilha

--- Notebook/algorithms/Estrutura de dados/pilha.cpp
+++ Notebook/algorithms/Estrutura de dados/pilha.cpp
@@ -11,4 +11,31 @@ int main()
     st.size(); // Retorna tamanho da pilha
     st.top(); // Retorna valor no topo da pilha
 
+    // Exemplo de delimitador com pilha
+    string n, resultado = "";
+    stack<char> st;
+    cin >> n;
+    for(size_t i = 0; i < n.length(); i++)
+    {
+        if(n[i] == '(')
+        {
+            st.push(n[i]);
+            resultado += '(';
+        }
+        else if(n[i] == ')' && st.size() > 0)
+        {
+            st.pop();
+            resultado += ')';
+        }
+        else if(n[i] == ')' && st.size() == 0)
+        {
+            resultado = '(' + resultado + ')';
+        }
+    }
+    while(st.size() > 0)
+    {
+        resultado += ')';
+        st.pop();
+    }
+    cout << resultado << '\n';
 }
